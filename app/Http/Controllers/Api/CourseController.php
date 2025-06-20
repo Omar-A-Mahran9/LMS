9471d704758dcd38ccc58a2640417f944d9af715
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-06-20 17:16:01 +0300

    firat commit

--- app/Http/Controllers/Api/CourseController.php
+++ app/Http/Controllers/Api/CourseController.php
@@ -65,13 +65,11 @@ if ($filter === 'my') {
           ->whereIn('course_student.status', ['approved', 'pending'])
           ->where('course_student.is_active', 1);
     });
-} elseif ($filter === 'other') {
-        $query->whereDoesntHave('students', function ($q) use ($student) {
-            $q->where('student_id', $student->id)
-            ->where('course_student.status', 'approved')
-            ->where('course_student.is_active', 1);
-        });
-    }
+}  elseif ($filter === 'other') {
+    $query->whereDoesntHave('students', function ($q) use ($student) {
+        $q->where('student_id', $student->id); // just not booked
+    });
+}
 
     }
 
