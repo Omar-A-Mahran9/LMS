1885573a20777bfb4b14d9d9111afae06505a90e
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-06-26 10:06:27 +0300

    firat commit

--- resources/views/dashboard/sections/show.blade.php
+++ resources/views/dashboard/sections/show.blade.php
@@ -1039,11 +1039,7 @@
                         {{ __('Correct') }}
                     </label>
                 </div>
-                <div class="col-md-2">
-                    <a href="javascript:;" data-repeater-delete class="btn btn-sm btn-danger">
-                        {{ __('Delete') }}
-                    </a>
-                </div>
+                
             </div>
         `);
 
