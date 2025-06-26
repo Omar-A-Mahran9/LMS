e44c3e3872cc3b7dc3fde62fb9355cf5e8ce5e6e
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-06-26 16:32:23 +0300

    firat commit

--- app/Http/Controllers/Dashboard/SectionController.php
+++ app/Http/Controllers/Dashboard/SectionController.php
@@ -20,7 +20,7 @@ class SectionController extends Controller
     {
           $this->authorize('view_sections');
         $courseId = $request->input('course_id');
- 
+
         $courses = Course::select('id', 'title_en', 'title_ar')->get();
         // Example static visited site count (you may want to make this dynamic)
         $visited_site = 10000;
@@ -28,9 +28,6 @@ class SectionController extends Controller
         if ($request->ajax()) {
                 if($courseId){
                     return response()->json(getModelData(model: new Section(), andsFilters: [['course_id', '=', (int)$courseId]],relations: ['course' => ['id', 'title_ar','title_en' ]]));
-
-                }else{
-                    return response()->json(getModelData(model: new Section(),relations: ['course' => ['id', 'title_ar','title_en' ]]));
 
                 }
         } else {
