87324e5ebac7352fc6bad055fa005599c20078d1
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-06-26 09:31:11 +0300

    firat commit

--- public/assets/dashboard/js/datatables/questions.js
+++ public/assets/dashboard/js/datatables/questions.js
@@ -239,21 +239,19 @@ var KTDatatablesServerSide = (function () {
                 $("#crud_modal").modal("show");
             });
         });
-
-        $("#form_repeater").repeater({
-            initEmpty: false,
-            isFirstItemUndeletable: true,
-            show: function () {
-                $(this).slideDown();
-                $(this).find("input").prop("readonly", false);
-            },
-
-            hide: function (deleteElement) {
-                $(this).slideUp(deleteElement);
-            },
-        });
     };
+    $("#form_repeater").repeater({
+        initEmpty: false,
+        isFirstItemUndeletable: true,
+        show: function () {
+            $(this).slideDown();
+            $(this).find("input").prop("readonly", false);
+        },
 
+        hide: function (deleteElement) {
+            $(this).slideUp(deleteElement);
+        },
+    });
     // Public methods
     return {
         init: function () {
