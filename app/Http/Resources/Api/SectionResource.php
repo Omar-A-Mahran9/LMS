3593f098ee884c22aa1e02cbb2c93d514055ccb8
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-06-28 07:44:28 +0300

    firat commit

--- app/Http/Resources/Api/SectionResource.php
+++ app/Http/Resources/Api/SectionResource.php
@@ -75,7 +75,7 @@ class SectionResource extends JsonResource
             }),
 
             // Optional section-level flags
-        'has_quizzes' => (!$quizAttemptLimitReached && $this->quizzes()->exists()) ? true : false,
+            'has_quizzes' => (!$quizAttemptLimitReached && $this->quizzes()->exists())  ? true : false,
             'has_homeworks' => $this->homeworks()->exists(),
             'quiz_required' => $hasAttemptedActiveQuiz ? 0 : $this->quiz_required,
 
