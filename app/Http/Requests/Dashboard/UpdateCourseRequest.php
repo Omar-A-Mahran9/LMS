6afd507b676628e4a54f70247d4cacecf76e7d80
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-06-28 07:04:55 +0300

    firat commit

--- app/Http/Requests/Dashboard/UpdateCourseRequest.php
+++ app/Http/Requests/Dashboard/UpdateCourseRequest.php
@@ -93,7 +93,7 @@ class UpdateCourseRequest extends FormRequest
     'certificate_available' => ['nullable', 'boolean'],
 
     // Duration & Dates
-    'start_date' => ['required', 'date', 'after_or_equal:today'],
+    'start_date' => ['required', 'date'],
     'end_date' => ['required', 'date', 'after_or_equal:start_date'],
 
 
