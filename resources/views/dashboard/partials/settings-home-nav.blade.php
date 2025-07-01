7c8c044a14ba580c06d1c4790316170b587d9000
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-07-01 07:51:18 +0300

    firat commit

--- resources/views/dashboard/partials/settings-home-nav.blade.php
+++ resources/views/dashboard/partials/settings-home-nav.blade.php
@@ -1,10 +1,12 @@
 <!--begin:::Tabs-->
 <ul class="nav nav-custom nav-tabs nav-line-tabs nav-line-tabs-2x border-0 fs-4 fw-semibold mb-n2 w-auto">
     <!--begin:::Tab item-->
+    <!--begin:::Tab item-->
     <li class="nav-item">
         <a class="nav-link text-active-primary pb-4 {{ getClassIfUrlContains('active', 'general') }}"
             href="{{ route('dashboard.settings.home.general') }}">{{ __('General') }}</a>
     </li>
+    <!--begin:::Tab item-->
     <!--begin:::Tab item-->
     <li class="nav-item">
         <a class="nav-link text-active-primary pb-4 {{ getClassIfUrlContains('active', 'banner') }}"
