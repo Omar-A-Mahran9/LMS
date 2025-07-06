2da8c8cfe8adc74dc3999ce9ed0a24abff20ef49
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-07-06 21:42:53 +0300

    firat commit

--- app/Http/Helpers/dashboard_functions.php
+++ app/Http/Helpers/dashboard_functions.php
@@ -393,11 +393,13 @@ if (!function_exists('generateCertificateForStudent')) {
             'certificateId'  => $certificateId,
             'certificateUrl' => $certificateUrl,
         ])
-        ->setOption('page-size', 'A4')
-        ->setOption('orientation', 'Landscape')
-        ->setOption('enable-local-file-access', true)
-        ->setOption('disable-smart-shrinking', true)
-        ->save($tempPath);
+    ->setOptions([
+            'page-size' => 'A4',
+            'orientation' => 'Landscape',
+            'enable-local-file-access' => true,
+            'disable-smart-shrinking' => true,
+        ])
+    ->save($tempPath);
                 // رفعه إلى مجلد attachments
         $storedFileName = uploadCertificatePdfFromPath($tempPath, $filename, 'Certificate');
 
