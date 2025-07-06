8e86c1a8b4d54af9a35a646ac60d199524d07c9e
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-07-06 20:13:56 +0300

    firat commit

--- app/Models/Certificate.php
+++ app/Models/Certificate.php
@@ -11,26 +11,14 @@ class Certificate extends Model
 
     protected $guarded = [];
 
-    protected $appends = ['name', 'full_image_path', 'description'];
+    protected $appends = [  ];
 
     protected $casts   = [
         'created_at' => 'date:Y-m-d',
         'updated_at' => 'date:Y-m-d',
     ];
 
-    // Return a generic name or a composed one
-    public function getNameAttribute()
-    {
-        return 'Certificate #' . $this->certificate_id;
-    }
 
-    public function getFullImagePathAttribute()
-    {
-        return $this->file_path ? asset($this->file_path) : null;
-    }
 
-    public function getDescriptionAttribute()
-    {
-        return "Certificate issued for student ID {$this->student_id} for course ID {$this->course_id}";
-    }
+ 
 }
