81c5836bcae7d3a0aff48312fa08c84ae2f3b3be
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-07-06 19:19:06 +0300

    firat commit

--- app/Http/Controllers/Api/CertificateController.php
+++ app/Http/Controllers/Api/CertificateController.php
@@ -54,7 +54,7 @@ class CertificateController extends Controller
                 return $this->failure(__('You must score at least 50% in all quizzes to receive a certificate.'));
             }
         }
-dd($quizIds,$failed);
+dd($quizIds);
         // ✅ Generate Certificate ID & QR Code
         $certificateId = 'CERT-' . strtoupper(Str::random(10));
         $certificateUrl = route('certificates.verify', ['id' => $certificateId]);
