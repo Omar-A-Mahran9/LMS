e533b5b1b24e8f46b29b7e2801d98d4e3438d5f9
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-07-06 22:03:12 +0300

    firat commit

--- app/Http/Controllers/Api/CertificateController.php
+++ app/Http/Controllers/Api/CertificateController.php
@@ -93,9 +93,7 @@ public function verify($id)
     // ✅ Return certificate info
     $fileUrl = getAttachmentPathFromDirectory($certificate->file_path, 'Certificate');
 
-        return $this->success('',[
-            // 'certificate_id'=> $certificate->certificate_id,
-            'file_url'      => $fileUrl,
-            // 'course_title'  => $course->title_en ?? $course->title,
-        ]);    }
+    return response()->file(public_path($certificate->file_path), [
+    'Content-Type' => 'application/pdf',
+]);   }
 }
