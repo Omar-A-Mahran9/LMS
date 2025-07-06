2b7ae63beb07dce7c1185f61ee7d2c07bae1f9e9
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-07-06 20:46:36 +0300

    firat commit

--- resources/views/certificates/certificate.blade.php
+++ resources/views/certificates/certificate.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <title>Certificate of Completion</title>
@@ -10,8 +9,7 @@
             margin: 0;
         }
 
-        html,
-        body {
+        html, body {
             margin: 0;
             padding: 0;
             font-family: DejaVu Sans, sans-serif;
@@ -22,87 +20,93 @@
         .certificate {
             width: 100%;
             height: 100%;
-            padding: 0;
-            border: 8px solid #00B2A9;
             box-sizing: border-box;
+            border: 8px solid #00B2A9;
             position: relative;
             text-align: center;
             overflow: hidden;
         }
 
         .content {
+            padding: 40px 60px 80px; /* top, sides, bottom */
             position: relative;
             z-index: 1;
-            padding: 40px 60px;
         }
 
         .logo {
             position: absolute;
             top: 30px;
-            left: 30px;
+            left: 40px;
             width: 100px;
             height: 100px;
-            object-fit: contain;
+            background-image: url('{{ asset(getImagePathFromDirectory(setting('logo_image'), 'Settings')) }}');
+            background-size: contain;
+            background-repeat: no-repeat;
+            background-position: center;
         }
 
         .qr {
             position: absolute;
             top: 30px;
-            right: 30px;
+            right: 40px;
             width: 100px;
             height: 100px;
         }
 
         .bg-text {
             position: absolute;
-            top: 50%;
+            top: 52%;
             left: 50%;
             transform: translate(-50%, -50%);
-            font-size: 140px;
+            font-size: 120px;
             color: #f0f0f0;
             font-weight: bold;
-            z-index: 0;
             white-space: nowrap;
+            z-index: 0;
         }
 
         .title {
-            font-size: 38px;
+            font-size: 36px;
             font-weight: bold;
-            margin-top: 60px;
+            margin-top: 80px;
+            margin-bottom: 20px;
         }
 
         .sub-title {
             font-size: 20px;
             margin: 12px 0;
+            color: #555;
         }
 
         .student-name {
             font-size: 30px;
             font-weight: bold;
             color: #00B2A9;
-            margin: 15px 0;
+            margin: 10px 0;
         }
 
         .course-title {
-            font-size: 24px;
+            font-size: 22px;
             margin: 20px 0;
+            font-style: italic;
         }
 
         .details {
             font-size: 14px;
-            margin-top: 20px;
+            margin-top: 30px;
+            color: #444;
         }
 
         .issued {
-            font-size: 14px;
-            margin-top: 20px;
-            color: #555;
+            font-size: 13px;
+            margin-top: 15px;
+            color: #666;
         }
 
         .signature {
             position: absolute;
             bottom: 30px;
-            right: 40px;
+            right: 50px;
             font-size: 14px;
             text-align: right;
         }
@@ -110,23 +114,20 @@
         .verify {
             position: absolute;
             bottom: 30px;
-            left: 40px;
+            left: 50px;
             font-size: 12px;
             color: #555;
             text-align: left;
         }
     </style>
-
 </head>
-
 <body>
     <div class="certificate">
         <div class="bg-text">CERTIFICATE</div>
 
         <div class="content">
             {{-- Logo --}}
-            <img src="{{ asset(getImagePathFromDirectory(setting('logo_image'), 'Settings')) }}" class="logo"
-                alt="Logo">
+            <div class="logo"></div>
 
             {{-- QR Code --}}
             <img src="data:image/png;base64,{{ $qrCode }}" class="qr" alt="QR Code">
@@ -164,5 +165,4 @@
         </div>
     </div>
 </body>
-
 </html>
