0720cd7bb24c59c40aab743592c312523a144ef6
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-07-06 19:47:45 +0300

    firat commit

--- app/Http/Controllers/Api/CertificateController.php
+++ app/Http/Controllers/Api/CertificateController.php
@@ -62,7 +62,7 @@ class CertificateController extends Controller
             'message' => __('You must score at least 50% in all quizzes. Retake: ') . implode(', ', $failedQuizzes)
         ], 403);
     }
-    $certificate = $this->generateCertificateForStudent($student, $course);
+    $certificate = generateCertificateForStudent($student, $course);
 
     return response()->file(public_path($certificate->file_path));
     }
