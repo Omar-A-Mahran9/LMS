7945ef591af1310a738e6c55321321f84e8f5641
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-07-06 20:13:27 +0300

    firat commit

--- app/Models/Certificate.php
+++ app/Models/Certificate.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Certificate extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    protected $appends = ['name', 'full_image_path', 'description'];
+
+    protected $casts   = [
+        'created_at' => 'date:Y-m-d',
+        'updated_at' => 'date:Y-m-d',
+    ];
+
+    // Return a generic name or a composed one
+    public function getNameAttribute()
+    {
+        return 'Certificate #' . $this->certificate_id;
+    }
+
+    public function getFullImagePathAttribute()
+    {
+        return $this->file_path ? asset($this->file_path) : null;
+    }
+
+    public function getDescriptionAttribute()
+    {
+        return "Certificate issued for student ID {$this->student_id} for course ID {$this->course_id}";
+    }
 }
