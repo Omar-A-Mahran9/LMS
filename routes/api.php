203ee80b88c9a846031634e123d4917efee00d1c
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-07-07 17:29:07 +0300

    firat commit

--- routes/api.php
+++ routes/api.php
@@ -72,7 +72,7 @@ Route::group(['middleware' => ['cors', 'json.response']], function () {
     Route::get('/sections/{id}/videos/count', [SectionVideoController::class, 'countBySection']);
     Route::get('/courses/{id}/sections/count', [SectionController::class, 'countSectionByCourse']);
 
-        Route::get('certificate/public/{id}', [CertificateController::class, 'publicDownload'])
+        Route::get('certificate/{id}', [CertificateController::class, 'publicDownload'])
     ->name('certificate.public');
     Route::post('login', 'Auth\AuthController@login');
     Route::post('login-otp/{customer:phone}', 'Auth\AuthController@loginOTP');
