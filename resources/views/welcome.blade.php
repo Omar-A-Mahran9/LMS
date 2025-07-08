ce26e8ddc110314874ee321ce764e2e472d59e31
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-07-09 00:39:40 +0300

    firat commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -173,7 +173,6 @@
                 <div class="d-flex flex-column align-items-center justify-content-center" style="min-height: 60vh;">
                     <img src="{{ getImagePathFromDirectory(setting('logo_image'), 'Settings') }}" alt="Logo"
                         style="max-width: 300px;" class="mb-5">
-                    <h3 class="text-muted">{{ __('Access Restricted') }}</h3>
                 </div>
             @endcan
 
