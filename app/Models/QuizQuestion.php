239891a9e6281cfa6244b4069b8a7581a5a47e48
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-07-17 20:07:21 +0300

    firat commit

--- app/Models/QuizQuestion.php
+++ app/Models/QuizQuestion.php
@@ -8,7 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class QuizQuestion extends Model
 {
     use HasFactory;
-
+    protected $guarded = [];
+    protected $appends = ['question'];
+    protected $casts   = [
+        'created_at' => 'date:Y-m-d',
+        'updated_at' => 'date:Y-m-d',
+    ];
        public function getQuestionAttribute()
         {
             return app()->getLocale() === 'ar' ? $this->question_ar : $this->question_en;
