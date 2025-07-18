67ad3b15036c6dcb8363398c0d3d3ea1b60d3aaa
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-07-18 18:53:53 +0300

    firat commit

--- app/Http/Controllers/Dashboard/SectionController.php
+++ app/Http/Controllers/Dashboard/SectionController.php
@@ -27,7 +27,7 @@ class SectionController extends Controller
                     return response()->json(getModelData(model: new Section(), orsFilters: [['course_id', (int) $courseId]],relations: ['course' => ['id', 'title_ar','title_en' ]]));
 
                 }else{
-                    return response()->json(getModelData(model: new Section(),relations: ['course' => ['id', 'title_ar','title_en' ]]));
+                    // return response()->json(getModelData(model: new Section(),relations: ['course' => ['id', 'title_ar','title_en' ]]));
 
                 }
         } else {
