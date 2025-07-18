321037e92b4ff6428012435e5c4ac740dbb395aa
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-07-18 18:56:28 +0300

    firat commit

--- app/Http/Controllers/Dashboard/SectionController.php
+++ app/Http/Controllers/Dashboard/SectionController.php
@@ -23,13 +23,12 @@ class SectionController extends Controller
         $visited_site = 10000;
 
         if ($request->ajax()) {
-                if($courseId){
-                    return response()->json(getModelData(model: new Section(), orsFilters: [['course_id', (int) $courseId]],relations: ['course' => ['id', 'title_ar','title_en' ]]));
+return response()->json(getModelData(
+    model: new Section(),
+    andsFilters: [['course_id', (int) $courseId]],
+    relations: ['course' => ['id', 'title_ar', 'title_en']]
+));
 
-                }else{
-                    // return response()->json(getModelData(model: new Section(),relations: ['course' => ['id', 'title_ar','title_en' ]]));
-
-                }
         } else {
 
             // Return the main view with data
