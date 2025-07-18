5c1d920fd2c9805aa80f4238cbca724e0bf7e53d
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-07-18 19:30:17 +0300

    firat commit

--- resources/views/dashboard/classes/index.blade.php
+++ resources/views/dashboard/classes/index.blade.php
@@ -214,7 +214,7 @@
                                     <input class="form-check-input " name="quiz_required" type="checkbox" value="1"
                                         id="quiz_required_switch">
                                     <span class="form-check-label text-dark"
-                                        for="quiz_required_switch">{{ __('quiz required"') }}</span>
+                                        for="quiz_required_switch">{{ __('quiz required') }}</span>
                                 </label>
                             </div>
 
