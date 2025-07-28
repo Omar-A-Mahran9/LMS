4dbe424258258bc0be84ea8fc02d625d1bb84354
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-07-29 02:23:54 +0300

    firat commit

--- app/Http/Controllers/Api/CourseController.php
+++ app/Http/Controllers/Api/CourseController.php
@@ -41,11 +41,11 @@ public function getCoursesByCategory(Request $request)
     $categoryId = $request->query('category_id');
     $perPage = $request->query('per_page', 10);
     $filter = $request->query('filter'); // values: 'my', 'other', or null
-//   if ($filter === 'my' && !Auth::guard('api')->check()) {
-//         $empty = Course::whereRaw('0=1')->paginate($perPage); // empty pagination
-//         $resource = CoursesDetailsResource::collection($empty)->response()->getData(true);
-//         return $this->successWithPagination('No courses found.', $resource);
-//     }
+  if ($filter === 'my' && !Auth::guard('api')->check()) {
+        $empty = Course::whereRaw('0=1')->paginate($perPage); // empty pagination
+        $resource = CoursesDetailsResource::collection($empty)->response()->getData(true);
+        return $this->successWithPagination('No courses found.', $resource);
+    }
     $query = Course::query()
         ->where('is_active', 1)
         ->where('is_class', 1)
@@ -72,12 +72,19 @@ public function getCoursesByCategory(Request $request)
           ->whereIn('course_student.status', ['approved', 'pending'])
           ->where('course_student.is_active', 1);
     });
-    } 
+        $courses = $query->paginate($perPage);
+
+    }  elseif ($filter === 'other') {
+    $query->whereDoesntHave('students', function ($q) use ($student) {
+        $q->where('student_id', $student->id); // just not booked
+    });
+        $courses = $query->paginate($perPage);
+
+    }
 
     }
 
 
-    $courses = $query->paginate($perPage);
     $resource = CoursesDetailsResource::collection($courses)->response()->getData(true);
 
     return $this->successWithPagination('Courses retrieved successfully.', $resource);
