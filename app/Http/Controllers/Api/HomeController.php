444ae1fd9e051fd8412cb84f59afb56a0cd35dd9
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-07-30 03:08:29 +0300

    firat commit

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -75,7 +75,7 @@ class HomeController extends Controller
                     : 0;
                 $full = $data['attempts'] * 100; // Adjust if quiz full mark is different
                 return [
-                    'name' => $data['student']->name,
+                    'name' => $data['student'],
                     'image' => $data['student']->full_image_path ,
                     'category' => $data['student']->category->name ?? 'N/A', // optional fallback
                     'average_score' => round($average, 2),
