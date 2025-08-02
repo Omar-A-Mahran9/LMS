02bf21d5e9b681d666bc40bb6e242cdccd232e19
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-08-03 01:54:39 +0300

    firat commit

--- app/Http/Controllers/Api/StudentQuizController.php
+++ app/Http/Controllers/Api/StudentQuizController.php
@@ -221,11 +221,11 @@ public function submitQuiz(Request $request, $quizAttemptId)
 
             if ($correctAnswer) {
                 similar_text(strtolower(trim($studentAnswer)), strtolower(trim($correctAnswer)), $percent);
-            //     dd(strtolower(trim($studentAnswer)),
-            //     strtolower(trim($correctAnswer)),
-            //     $percent,
-            //     similar_text(strtolower(trim($studentAnswer)), strtolower(trim($correctAnswer)), $percent)
-            // );
+                dd(strtolower(trim($studentAnswer)),
+                strtolower(trim($correctAnswer)),
+                $percent,
+                similar_text(strtolower(trim($studentAnswer)), strtolower(trim($correctAnswer)), $percent)
+            );
 
                 if ($percent >= 80) {
                     $score += $question->points;
