01ed41f20a37bd86d2ff68ad96969119ff06e27e
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-08-03 03:32:25 +0300

    firat commit

--- app/Http/Controllers/Api/StudentQuizController.php
+++ app/Http/Controllers/Api/StudentQuizController.php
@@ -364,7 +364,7 @@ public function results($studentQuizId)
 
             $pointsAwarded = $isCorrect ? $question->points : 0;
             $totalScore += $pointsAwarded;
-dd($studentAnswer);
+dd($attemptAnswer);
             $results[] = [
                 'question_id'      => $question->id,
                 'question_type'      => $question->type,
