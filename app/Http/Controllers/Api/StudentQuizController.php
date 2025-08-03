3f7e13ff0fc873781a344dd3a3ca6196d72aa6a5
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-08-03 03:29:36 +0300

    firat commit

--- app/Http/Controllers/Api/StudentQuizController.php
+++ app/Http/Controllers/Api/StudentQuizController.php
@@ -364,11 +364,11 @@ public function results($studentQuizId)
 
             $pointsAwarded = $isCorrect ? $question->points : 0;
             $totalScore += $pointsAwarded;
-
+dd($question);
             $results[] = [
                 'question_id'      => $question->id,
                 'question_type'      => $question->type,
-                'answer_percent' => $question->answer_percent . '%',
+                'answer_percent' => round($question->answer_percent) . '%',
 
                 'question'         => $question->question,
                 'question_answers' => $question->answers->map(fn($ans) => [
