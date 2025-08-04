c11d64e0942cf441d8d9141715354880fb414b5c
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-08-04 19:31:25 +0300

    firat commit

--- app/Http/Controllers/Api/CourseController.php
+++ app/Http/Controllers/Api/CourseController.php
@@ -221,7 +221,7 @@ public function getVideosByClass($id, Request $request)
     $class = CourseClass::where('id', $id)
         ->where('is_active', 1)
         ->first();
-dd($class);
+dd($id);
     if (!$class) {
         return $this->failure('Class not found or unpublished');
     }
