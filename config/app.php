111be2f92a8e9100b41d193efe80e01779a8c30d
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-08-04 20:00:39 +0300

    firat commit

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://admin.mohamed-elnagar.com'),
 
     'asset_url' => env('ASSET_URL'),
     'oto_refresh_token' => env('OTO_REFRESH_TOKEN', 'default_value_if_not_set'),
