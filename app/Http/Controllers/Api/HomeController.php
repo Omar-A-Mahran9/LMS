0acfdd2623519186e637c2099243a6ea23297cba
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-08-08 14:51:05 +0300

    firat commit

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -155,7 +155,7 @@ foreach ($category->courses as $course) {
                 $studentId = $student->id;
                 // Initialize stats if student not yet tracked
 
-           
+           dd($studentStats[$studentId]);
 
                 // Accumulate stats
                 $studentStats[$studentId]['total_score'] += $attempt->score;
