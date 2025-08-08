5c62ed95ff664d864464389907e98730205af187
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-08-08 14:53:18 +0300

    firat commit

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -155,14 +155,14 @@ foreach ($category->courses as $course) {
                 $studentId = $student->id;
                 // Initialize stats if student not yet tracked
 
-                if (!isset($studentStats[$studentId])) {
-                    $studentStats[$studentId] = [
-                        'total_score' => 0,
-                        'total_possible' => 0,
-                        'attempts' => 0,
-                        'student' => $student,
-                    ];
-                }
+                // if (!isset($studentStats[$studentId])) {
+                //     $studentStats[$studentId] = [
+                //         'total_score' => 0,
+                //         'total_possible' => 0,
+                //         'attempts' => 0,
+                //         'student' => $student,
+                //     ];
+                // }
 dd( $studentStats[$studentId]);
                 // Accumulate stats
                 $studentStats[$studentId]['total_score'] += $attempt->score;
