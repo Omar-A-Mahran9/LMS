af3392676d20da202f45855e4158645a9716811d
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-08-14 16:29:34 +0300

    firat commit

--- resources/views/dashboard/lives/index.blade.php
+++ resources/views/dashboard/lives/index.blade.php
@@ -142,7 +142,7 @@
                                     <option value=""></option>
                                     @foreach ($courses as $course)
                                         <option value="{{ $course->id }}">
-                                            {{ $course->title . '-' . ($course->class ? $course->class->title : '') }}
+                                            {{ $course->title   }}
                                         </option>
                                     @endforeach
                                 </select>
