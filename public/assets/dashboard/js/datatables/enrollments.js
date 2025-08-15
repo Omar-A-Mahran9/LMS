2d882bd211728587554d6607f3be559d96afd0b0
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-08-15 19:09:01 +0300

    firat commit

--- public/assets/dashboard/js/datatables/enrollments.js
+++ public/assets/dashboard/js/datatables/enrollments.js
@@ -34,7 +34,7 @@ var KTDatatablesServerSide = (function () {
                 { data: "payment_type" }, // payment type
                 { data: "status" }, // status
                 { data: "created_at" }, // enrollment date
-                {  data: "category.name" }, // category
+                {  data: "course.category.name" }, // category
 
                 { data: null }, // actions
             ],
