0a0a1abe5a87298e4e56574f55495259187ba79c
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-08-15 14:40:28 +0300

    firat commit

--- app/Http/Controllers/Dashboard/ClassController.php
+++ app/Http/Controllers/Dashboard/ClassController.php
@@ -24,30 +24,24 @@ public function index(Request $request)
 
     // AJAX request
     if ($request->ajax()) {
-if ($courseId) {
-    return response()->json(
-        getModelData(
-            model: new CourseClass(),
-            andsFilters: [['course_id', '=', $courseId]],
-            relations: [
-                'course' => ['id', 'title_ar', 'title_en', 'category_id'],
-                // 'course.category' => ['id', 'name_ar', 'name_en']
-            ]
-        )
-    );
-} else {
-    return response()->json(
-        getModelData(
-            model: new CourseClass(),
-            relations: [
-                'course' => ['id', 'title_ar', 'title_en', 'category_id'],
-                // 'course.category' => ['id', 'name_ar', 'name_en']
-            ]
-        )
-    );
-}
-
-
+        if ($courseId) {
+            // Filter by course_id
+            return response()->json(
+                getModelData(
+                    model: new CourseClass(),
+                    andsFilters: [['course_id', '=', $courseId]],
+                    relations: ['course' => ['id', 'title_ar', 'title_en']]
+                )
+            );
+        } else {
+            // No filter
+            return response()->json(
+                getModelData(
+                    model: new CourseClass(),
+                    relations: ['course' => ['id', 'title_ar', 'title_en']]
+                )
+            );
+        }
     }
 
     // Regular page load
