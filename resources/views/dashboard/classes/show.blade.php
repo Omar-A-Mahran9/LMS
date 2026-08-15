bb5b73c4ffe817a268b3aa1d81cdbc84d5d5c63c
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2026-08-15 13:23:24 +0300

    Add benchmark results

--- resources/views/dashboard/classes/show.blade.php
+++ resources/views/dashboard/classes/show.blade.php
@@ -199,6 +199,30 @@
                                         </button>
                                         <!--end::Add customer-->
                                     @endif
+
+                                    @if ($hasReadingPassages)
+                                        <div id="add_btn_reading_passage" data-bs-toggle="modal"
+                                            data-bs-target="#crud_reading_passage_modal" data-kt-docs-table-toolbar="base"
+                                            for="kt_datatable">
+
+                                            <button type="button" class="btn btn-primary" id="reading_passage"
+                                                data-bs-toggle="tooltip" data-kt-initialized="1">
+                                                <!--begin::Svg Icon | path: icons/duotune/arrows/arr075.svg-->
+                                                <span class="svg-icon svg-icon-2">
+                                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                                        xmlns="http://www.w3.org/2000/svg">
+                                                        <rect opacity="0.5" x="11.364" y="20.364" width="16"
+                                                            height="2" rx="1"
+                                                            transform="rotate(-90 11.364 20.364)" fill="currentColor">
+                                                        </rect>
+                                                        <rect x="4.36396" y="11.364" width="16" height="2"
+                                                            rx="1" fill="currentColor"></rect>
+                                                    </svg>
+                                                </span>
+                                                <!--end::Svg Icon-->{{ __('Add new reading passage') }}
+                                            </button>
+                                        </div>
+                                    @endif
                                 </div>
                                 <!--end::Toolbar-->
                             @endcan <!--begin::Datatable-->
@@ -361,6 +385,8 @@
                                     </div>
                                 </div>
 
+
+
                                 <div class="modal-body">
 
                                     {{-- Course & Section --}}
@@ -467,28 +493,7 @@
                         </div>
                     </div>
                 </form>
-                ddnssjddnndndnddnndndndndd
-                @if ($hasReadingPassages)
-                    <div id="add_btn_reading_passage" data-bs-toggle="modal" data-bs-target="#crud_reading_passage_modal"
-                        data-kt-docs-table-toolbar="base" for="kt_datatable">
 
-                        <button type="button" class="btn btn-primary" id="reading_passage" data-bs-toggle="tooltip"
-                            data-kt-initialized="1">
-                            <!--begin::Svg Icon | path: icons/duotune/arrows/arr075.svg-->
-                            <span class="svg-icon svg-icon-2">
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <rect opacity="0.5" x="11.364" y="20.364" width="16" height="2"
-                                        rx="1" transform="rotate(-90 11.364 20.364)" fill="currentColor">
-                                    </rect>
-                                    <rect x="4.36396" y="11.364" width="16" height="2" rx="1"
-                                        fill="currentColor"></rect>
-                                </svg>
-                            </span>
-                            <!--end::Svg Icon-->{{ __('Add new reading passage') }}
-                        </button>
-                    </div>
-                @endif
                 <form id="crud_reading_passage_form" class="ajax-form w-75"
                     action="{{ route('dashboard.reading_passage.store') }}" method="post" enctype="multipart/form-data"
                     data-success-callback="onAjaxSuccess" data-error-callback="onAjaxError">
