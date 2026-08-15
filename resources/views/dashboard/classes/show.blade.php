18f668ff0ec596eb0883c7302827839da56ac206
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2026-08-15 13:49:14 +0300

    Add benchmark results

--- resources/views/dashboard/classes/show.blade.php
+++ resources/views/dashboard/classes/show.blade.php
@@ -230,40 +230,42 @@
                                         </button>
                                     @endif
 
-                                    @if ($hasReadingPassages)
-                                        <div id="add_btn_reading_passage" data-bs-toggle="modal"
-                                            data-bs-target="#crud_reading_passage_modal" data-kt-docs-table-toolbar="base">
-
-                                            <button type="button" class="btn btn-primary" id="reading_passage"
-                                                data-bs-toggle="tooltip" data-kt-initialized="1">
-
-                                                <span class="svg-icon svg-icon-2">
-
-                                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                                        xmlns="http://www.w3.org/2000/svg">
-
-                                                        <rect opacity="0.5" x="11.364" y="20.364" width="16"
-                                                            height="2" rx="1"
-                                                            transform="rotate(-90 11.364 20.364)" fill="currentColor">
-                                                        </rect>
-
-                                                        <rect x="4.36396" y="11.364" width="16" height="2"
-                                                            rx="1" fill="currentColor">
-                                                        </rect>
-
-                                                    </svg>
-
-                                                </span>
-
-                                                {{ __('Add new reading passage') }}
-
-                                            </button>
-
-                                        </div>
-                                    @endif
 
                                 </div>
                             @endcan
+
+
+                            @if ($hasReadingPassages)
+                                <div id="add_btn_reading_passage" data-bs-toggle="modal"
+                                    data-bs-target="#crud_reading_passage_modal" data-kt-docs-table-toolbar="base">
+
+                                    <button type="button" class="btn btn-primary" id="reading_passage"
+                                        data-bs-toggle="tooltip" data-kt-initialized="1">
+
+                                        <span class="svg-icon svg-icon-2">
+
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                                xmlns="http://www.w3.org/2000/svg">
+
+                                                <rect opacity="0.5" x="11.364" y="20.364" width="16" height="2"
+                                                    rx="1" transform="rotate(-90 11.364 20.364)"
+                                                    fill="currentColor">
+                                                </rect>
+
+                                                <rect x="4.36396" y="11.364" width="16" height="2" rx="1"
+                                                    fill="currentColor">
+                                                </rect>
+
+                                            </svg>
+
+                                        </span>
+
+                                        {{ __('Add new reading passage') }}
+
+                                    </button>
+
+                                </div>
+                            @endif
 
                         </div>
 
